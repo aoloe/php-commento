aebe74d9046c7927fab077549ee4806665a59c4d
ale rimoldi <ale@graphicslab.org>
2017-05-18 22:19:39 +0200

    add the create path and remove the keys from the list of comments

--- sample/index.php
+++ sample/index.php
@@ -9,7 +9,7 @@ ini_set('display_startup_errors', true);
 
 include_once('src/commento.php');
 
-if (!loadConfiguration()) {
+if (!Commento\loadConfiguration()) {
     // $resultType = 'html';
     // $result = "<p>Cannot load the configuration file.</p>";
 }
@@ -19,30 +19,55 @@ $resultType = 'json';
 $urlParameter = parse_url(substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['PHP_SELF']).'/')));
 // debug('urlParameter', $urlParameter);
 
-if ($urlParameter['path'] === 'get') {
-    $comments = [];
-    $message = '';
-    if (array_key_exists('url', $_REQUEST)) {
-        $comments = getComments($_REQUEST['url']);
-    } else {
-        $message = 'no url defined';
-    }
-    $result = [
-        'success' => true,
-        'message' => $message,
-        'comments' => $comments,
-    ];
-} elseif ($urlParameter['path'] === 'create') {
-    // TODO: all html escaped; formatting uses markdown (on create or on get?)
-} elseif ($urlParameter['path'] === 'install') {
-    $resultType = 'html';
-    install();
-    $result = "<p>Installation successful</p>";
-} else {
-    $result = [
-        'success' => false,
-        'message' =>'path not found',
-    ];
+switch($urlParameter['path']) {
+    case 'get':
+        $comments = [];
+        $message = '';
+        if (array_key_exists('url', $_REQUEST)) {
+            $comments = array_values(Commento\getComments($_REQUEST['url']));
+        } else {
+            $message = 'no url defined';
+        }
+        $result = [
+            'success' => true,
+            'message' => $message,
+            'comments' => $comments,
+        ];
+    break;
+    case 'create':
+        // TODO: all html escaped; formatting uses markdown (on create or on get?)
+        $success = false;
+        if (array_key_exists('url', $_REQUEST)
+         && array_key_exists('comment', $_REQUEST)
+         && array_key_exists('name', $_REQUEST)
+         && array_key_exists('parent', $_REQUEST)
+        ) {
+            $success = Commento\addComment(
+                $_REQUEST['url'],
+                [
+                    'url' => $_REQUEST['url'],
+                    'comment' => $_REQUEST['comment'],
+                    'name' => $_REQUEST['name'],
+                    'parent' => $_REQUEST['parent'],
+                ]
+            );
+        }
+        $result = [
+            'success' => $success,
+            'message' => null,
+            'comments' => [],
+        ];
+    break;
+    case 'install':
+        $resultType = 'html';
+        Commento\install();
+        $result = "<p>Installation successful</p>";
+    break;
+    default:
+        $result = [
+            'success' => false,
+            'message' =>'path not found',
+        ];
 }
 
 if ($resultType === 'json') {
