d8ca55ee9441b6e225a1acfeffc695cf8cdf8e1a
ale rimoldi <ale@graphicslab.org>
2017-07-28 18:21:44 +0200

    clean up the sample index.php and move the debug instruction to the new index-sample.php

--- sample/index-debug.php
+++ sample/index-debug.php
@@ -1,11 +1,25 @@
 <?php
 
+// error_reporting(E_ALL);
+// ini_set('display_errors', true);
+// ini_set('display_startup_errors', true);
+
 include_once('commento.php');
+
+// debug('_REQUEST', $_REQUEST);
+// debug('_SERVER', $_SERVER);
+
 
 define('COMMENTO_HAS_CONFIG_FILE', false);
 define('COMMENTO_DATA_PATH', dirname($_SERVER['DOCUMENT_ROOT']).'/php-commento-data');
 
 Commento\loadConfiguration();
+
+// if (!Commento\loadConfiguration()) {
+//     // $resultType = 'html';
+//     // $result = "<p>Cannot load the configuration file.</p>";
+// }
+
 
 $urlParameter = parse_url(substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['PHP_SELF']).'/')));
 
